e08e3042bec68c62b02122da6260b4626b24dbad
Hao <pockyhao518@gmail.com>
2021-02-01 01:53:51 -0500

    tweet

---
+++ app.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+mongoose.set('useUnifiedTopology', true);
+
+const express = require("express");
+const app = express();
+
+const db = require('./config/keys').mongoURI;
+mongoose
+    .connect(db, { useNewUrlParser: true })
+    .then(() => console.log("Connected to MongoDB successfully"))
+    .catch(err => console.log(err));
+
+app.get("/", (req, res) => res.send("Hello World"));
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
+
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const passport = require('passport');
+app.use(passport.initialize());
+require('./config/passport')(passport);
